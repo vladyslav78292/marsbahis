b50a81ffd71e8896113bb77313a65f6fa5f37689
Vladyslav Horovyi <vladyslav.horovyi.dev@gmail.com>
2023-08-17 17:08:28 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,7 @@
 'use strict';
+
+const block2 = document.querySelector('.block2');
+const block1 = document.querySelector('.block1');
+
+const height_block1 = block1.offsetHeight;
+block2.style.marginTop = height_block1 + 'px';
